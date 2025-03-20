6939f520887353b660eefb717a3c948123541595
Saahiti <kssaahitimbl@gmail.com>
2025-03-20 21:59:50 +0000

    Initial commit - Kubernetes Monitoring with Prometheus & Grafana

---
+++ order-service/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    return jsonify({"orders": ["Order1", "Order2", "Order3"]})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
+
